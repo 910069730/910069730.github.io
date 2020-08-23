b447397d749a0ea6ed05c3dd936212e70aafd1a4
910069730 <910069730@qq.com>
2020-08-23 18:11:02 +0800

    Site updated: 2020-08-23 18:10:57

--- js/script.js
+++ js/script.js
@@ -92,25 +92,25 @@ $('body').on('click', function(){
 });
 
 // Caption
-// $('.article-entry').each(function(i){
-//  $(this).find('img').each(function(){
-//    if ($(this).parent().hasClass('fancybox')) return;
+$('.article-entry').each(function(i){
+ $(this).find('img').each(function(){
+   if ($(this).parent().hasClass('fancybox')) return;
 
-//    var alt = this.alt;
+   var alt = this.alt;
 
-//    if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+   if (alt) $(this).after('<span class="caption">' + alt + '</span>');
 
-//    $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
-//  });
+   $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+ });
 
-//  $(this).find('.fancybox').each(function(){
-//    $(this).attr('rel', 'article' + i);
-//  });
-// });
+ $(this).find('.fancybox').each(function(){
+   $(this).attr('rel', 'article' + i);
+ });
+});
 
-// if ($.fancybox){
-//  $('.fancybox').fancybox();
-// }
+if ($.fancybox){
+ $('.fancybox').fancybox();
+}
 
 // Mobile nav
 var $container = $('#container'),
